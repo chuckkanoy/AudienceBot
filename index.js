c6cbf3b0a24ffac6380b7421807a7cf57c8fdc9e
Charles Kanoy <ch.kanoy@gmail.com>
2021-03-08 16:56:53 -0600

    added env to gitignore

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-//RUNNING: node --experimental-json-modules index.js
-
-// const config = require('./config.json');
 import config from './config.json';
 import {getGame} from './games.js';
 import {parseSuggestions, getSuggestion} from './suggestions.js';
 import {getHelp} from './help.js';
-// const improv = require('./improv.json');
-// import './improv.json';
-// const games = require('./games.js');
 import {} from 'dotenv/config.js';
 
 import Discord from 'discord.js';
-// const Discord = require('discord.js');
 const client = new Discord.Client();
 
 function separate(message) {
@@ -61,42 +54,5 @@ client.on('message', (message) => {
         }
     }
 });
-
-// function getTest(game) {
-//     const warmups = improv.games.warmups;
-//     const tweeners = improv.games.tweeners;
-//     const competitives = improv.games.competitives;
-//     const fivePerson = improv.games['5PersonScenes'];
-//     const fourPerson = improv.games['4PersonScenes'];
-//     const threePerson = improv.games['3PersonScenes'];
-//     const longs = improv.games.longForms;
-//     const gameArr = [warmups, tweeners, competitives, fivePerson,
-//         fourPerson, threePerson, longs];
-//     let search = [];
-
-//     switch(game) {
-//         case "warmup": search = warmups;
-//             break;
-//         case "tweener": search = tweeners;
-//             break;
-//         case "competitives": search = competitives;
-//             break;
-//         case "5 person scene":
-//         case "five person scene": search = fivePerson;
-//             break;
-//         case "4 person scene":
-//         case "four person scene": search = fourPerson;
-//             break;
-//         case "3 person scene":
-//         case "three person scene": search = threePerson;
-//             break;
-//         case "long":
-//         case "long form": search = longs;
-//             break;
-//         default: search = gameArr[Math.floor((Math.random() * (gameArr.length - 1)) + 0)];
-//     }
-
-//     return search[Math.floor((Math.random() * (search.length - 1)) + 0)];
-// }
 
 client.login(process.env.TOKEN);
